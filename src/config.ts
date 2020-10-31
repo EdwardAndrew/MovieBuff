59160b9500cbb9e7580e21e334127d5dddbaa51c
Ed Andrew <ed.andrew95@gmail.com>
2020-10-31 16:06:40 +0000

    Increase timeout

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ export const config = {
         BASE_URL: "https://api.igdb.com/v4",
     },
     DISCORD_TOKEN: process.env['DISCORD_TOKEN'],
-    API_TIMEOUT: 2500,
+    API_TIMEOUT: 4000,
     CACHE_NOT_FOUND_TTL: 24 * 60 * 60,
     MAX_DESCRIPTION_LENGTH: 990,
 }
